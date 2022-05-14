169b9319e6e9bac60a03371bcb8678d6de30375a
Efraim Calebe Mertens <mertensefraim@gmail.com>
2022-05-13 21:54:58 -0300

    Exercicio 1 For, TryCatch Exercicio 1: testar o funcionamento....

---
+++ ExerciciosForTryCatch/Exercicio1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosForTryCatch
+{
+    internal class Exercicio1
+    {
+        public void Executar()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                var numeroValido = false;
+                while (numeroValido == false)
+                {
+                    try
+                    {
+
+                        Console.Write("Digite o nome da peça: ");
+                        var nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite o preço da " + i + "° peça: ");
+                        var preco = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Valor digitado inválido!");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ExerciciosForTryCatch/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExerciciosForTryCatch;
+
+Console.WriteLine(@"1 - Exercicio 1
+
+");
+int escolha = Convert.ToInt32(Console.ReadLine());
+if (escolha == 1)
+{
+    Exercicio1 Exercicio1 = new Exercicio1();
+    Exercicio1.Executar();
+}
