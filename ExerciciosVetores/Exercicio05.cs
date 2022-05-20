88aa164472ea673660e61f4248ce053e47d6ce79
Samuel Lopes <samlowand@gmail.com>
2022-05-20 19:37:36 -0300

    Exercicio 5 completo Nova opção no Menu Principal

---
+++ ExerciciosVetores/Exercicio05.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosVetores
+{
+    internal class Exercicio05
+    {
+        public void Executar()
+        {
+            double[] pesos = new double[5];
+            double somaPesos = 0;
+            for (var i = 0;i<pesos.Length;i++)
+            {
+                Console.Write("Digite o [" + (i + 1) + "] peso: ");
+                pesos[i] = Convert.ToDouble(Console.ReadLine());
+                somaPesos = somaPesos + pesos[i];
+            }
+            Console.WriteLine("Soma total dos Pesos: " + somaPesos);
+            Console.WriteLine("Média do Pesos: " + (somaPesos / pesos.Length));
+        }
+    }
+}
--- ExerciciosVetores/Program.cs
+++ ExerciciosVetores/Program.cs
@@ -4,7 +4,8 @@ Console.WriteLine(@"----------------MENU----------------
 1 - Exercicio 01
 2 - Exercicio 02
 3 - Exercicio 03
-4 - Exercicio 04");
+4 - Exercicio 04
+5 - Exercicio 05");
 int escolha = Convert.ToInt32(Console.ReadLine());
 
 if(escolha == 1)
@@ -26,4 +27,9 @@ else if(escolha == 4)
 {
     Exercicio04 Exercicio04 = new Exercicio04();
     Exercicio04.Executar();
+}
+else if (escolha == 5)
+{
+    Exercicio05 Exercicio05 = new Exercicio05();
+    Exercicio05.Executar();
 }
