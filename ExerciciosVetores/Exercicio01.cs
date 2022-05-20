4313effa4623a3f729798ca34362142dbfe85892
Samuel Lopes <samlowand@gmail.com>
2022-05-20 18:34:03 -0300

    Exercicio 1 Completo Adicionado Menu Principal em Program

---
+++ ExerciciosVetores/Exercicio01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosVetores
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            var numeros = new int[16];
+            int soma = 0;
+            for (int i = 0; i <numeros.Length;i++)
+            {
+                Console.WriteLine("Digite o [" + (i + 1) + "] valor:");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+                soma = soma + numeros[i];
+            }
+            Console.WriteLine("Soma de todos os valores: " + soma);
+        }
+    }
+}
---
+++ ExerciciosVetores/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExerciciosVetores;
+
+Console.WriteLine(@"----------------MENU----------------
+1 - Exercicio 01");
+int escolha = Convert.ToInt32(Console.ReadLine());
+
+if(escolha == 1)
+{
+    Exercicio01 Exercicio01 = new Exercicio01();
+    Exercicio01.Executar();
+}
