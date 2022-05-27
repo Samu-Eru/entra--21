11017e85fd221e41b0a88df68f1730c5a5b0c167
Samuel Lopes <samlowand@gmail.com>
2022-05-26 22:09:58 -0300

    Exercicio 01 Lista 5 incompleto

---
+++ ExerciciosOrientacaoObjetos/Exercicio01/ExemploLivro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class ExemploLivro
+    {
+        public void Executar()
+        {
+            Livro livro1 = new Livro();
+            livro1.Titulo = "a volta dos que não foram";
+            livro1.Autor = "Austin";
+            livro1.QuantidadePaginas = 503;
+            livro1.QuantidadePaginasLidas = 209;
+            livro1.QuantidadeReleituras = 0;
+
+         
+
+        }
+    }
+}
---
+++ ExerciciosOrientacaoObjetos/Exercicio01/Livro.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosOrientacaoObjetos
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public string CódigoISBN;
+        public string IdiomaOriginal;
+        public DateTime DataLançamento;
+        public int QuantidadePaginas;
+        public int QuantidadePaginasLidas;
+        public int QuantidadeReleituras;
+
+        public void ApresentarTituloAutor()
+        {
+            Console.WriteLine("Nome Título: " + Titulo);
+            Console.WriteLine("Nome Autor: " + Autor);
+        }
+
+        public int PaginasParaLer()
+        {
+            var totalDePaginas = QuantidadePaginas;
+            var paginasLidas = QuantidadePaginasLidas;
+
+            return (QuantidadePaginas - QuantidadePaginasLidas);
+        }
+
+        public int TotalVezesLidas()
+        {
+            var vezesLidas = QuantidadeReleituras;
+            return vezesLidas;
+        }
+        public int TotalPaginasLidas()
+        {
+
+            var paginasLidas = QuantidadePaginasLidas;
+            var vezesLidas = TotalVezesLidas();
+            var quantidadePaginas = QuantidadePaginas;
+
+            if ((vezesLidas == 0) || (vezesLidas == 1))
+            {
+                return paginasLidas;
+            }
+            else
+            {
+                return paginasLidas + (quantidadePaginas * vezesLidas);
+            }
+
+        }
+    }
+}
